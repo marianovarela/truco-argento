bdc84698cea394f1caa004e0974ec0d10eb5d984
Mariano Varela <marianogvarela@gmail.com>
2014-12-31 12:29:27 -0300

    trabajando en la transición entre el tablero y el modal

--- model/truco.js
+++ model/truco.js
@@ -26,6 +26,153 @@
 	
 	$(function(){component.main_left.set_active(true);});
 	
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	component.control = (function() {
+	  var Control, get_keycode, keycodes, listeners;
+	  listeners = [];
+	  keycodes = [
+	    {
+	      key: 'left',
+	      code: 37
+	    }, {
+	      key: 'up',
+	      code: 38
+	    }, {
+	      key: 'right',
+	      code: 39
+	    }, {
+	      key: 'down',
+	      code: 40
+	    }, {
+	      key: 'enter',
+	      code: 13
+	    }
+	  ];
+	  get_keycode = function(code) {
+	    var keycode, _i, _len;
+	    for (_i = 0, _len = keycodes.length; _i < _len; _i++) {
+	      keycode = keycodes[_i];
+	      if (keycode.code === code) {
+	        return keycode;
+	      }
+	    }
+	  };
+	  return Control = (function() {
+	    function Control() {}
+	
+	    Control.subscribe = function(listener) {
+	      listeners.push(listener);
+	      return function() {
+	        return unsubscribe(listener);
+	      };
+	    };
+	
+	    Control.unsubscribe = function(listener) {
+	      return _.remove(listeners, listener);
+	    };
+	
+	    Control.initialize = function() {
+	      return document.onkeydown = function(event) {
+	        var keycode, listener, _i, _len;
+	        if (keycode = get_keycode(event.keyCode)) {
+	          for (_i = 0, _len = listeners.length; _i < _len; _i++) {
+	            listener = listeners[_i];
+	            listener.handle(keycode.key);
+	          }
+	          event.preventDefault();
+	          return false;
+	        }
+	      };
+	    };
+	
+	    return Control;
+	
+	  })();
+	})(this);
+
+	function iniciarDialog(){
+		$(function(){component.main_left.set_not_active();});
+		$(function(){component.exit.set_active(true);});
+		console.log(component);	
+	}
+
+	function reiniciarTablero(){
+		$('#Envido').on('setearActivo', function(){component.main_left.set_active(true); component.exit.set_not_active();});
+		$('#Envido').trigger('setearActivo');
+	}
+	
+	function setearActivo(){
+		console.log("lalalala");
+	}
+	
+	function iniciarTablero() {
+		
+		component.control.initialize();
+		component.control.subscribe(component.root);
+		
+		component.main_left = extensible.create_vertical('main-left');
+		
+		
+		component.exit = extensible.create_horizontal('exit');
+		
+		component.root
+			.add(extensible.create_horizontal('dialog')
+			    .add(component.exit.set_priority(0)
+			      .add(extensible.create_horizontal('end').set_priority(0)
+			        .add(extensible.create_leaf('Reiniciar-dialog').set_priority(0))
+			        .add(extensible.create_leaf('Salir-dialog').set_priority(1))
+			      )
+			    )
+			);
+		
+		component.root
+		  .add(extensible.create_horizontal('main')
+		    .add(component.main_left.set_priority(0)
+		      .add(extensible.create_horizontal('mentira').set_priority(0)
+		        .add(extensible.create_leaf('Envido').set_priority(0))
+		        .add(extensible.create_leaf('RealEnvido').set_priority(1))
+		        .add(extensible.create_leaf('FaltaEnvido').set_priority(2))
+		      )
+		      .add(extensible.create_horizontal('rabon').set_priority(1)
+		        .add(extensible.create_leaf('IrAlMazo').set_priority(0))
+		        .add(extensible.create_leaf('Truco').set_priority(1))
+		        .add(extensible.create_leaf('reTruco').set_priority(2))
+		        .add(extensible.create_leaf('vale4').set_priority(3))
+		      )
+		      .add(extensible.create_horizontal('respuesta').set_priority(2)
+		        .add(extensible.create_leaf('Quiero').set_priority(0))
+		        .add(extensible.create_leaf('NoQuiero').set_priority(1))
+		        .add(extensible.create_leaf('Reiniciar-left').set_priority(2))
+		      )
+		      
+		    )
+		    .add(extensible.create_horizontal('main-center').set_priority(1)
+		    	.add(extensible.create_leaf('naipe-0').set_priority(0))
+		        .add(extensible.create_leaf('naipe-1').set_priority(1))
+		        .add(extensible.create_leaf('naipe-2').set_priority(2))
+		    )
+		    .add(extensible.create_vertical('main-right').set_priority(2)
+		    	.add(extensible.create_leaf('opciones').set_priority(0))
+		    	.add(extensible.create_horizontal('menu').set_priority(1)
+		    		.add(extensible.create_leaf('Reiniciar-right').set_priority(0))
+		    		.add(extensible.create_leaf('Salir').set_priority(1))
+		    	)
+		    )
+		);
+	}
+
+	iniciarTablero();
+	
+	function sacarCarta(indice){
+		var componentes = component.root.components[0].components[1];
+		console.log(componentes);
+		componentes.remove_leaf("naipe-1");
+		console.log(componentes);
+	}	
+	
+	
 	function setUp(){
 		_partidaActual = new Partida();
 		_partidaActual.iniciar('Jugador', 'Computadora');
@@ -1535,147 +1682,6 @@
 		}
 	};
 	
-	
-	/////////////////////////////////////////////////////////////////////////////////////////////////////
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-	component.control = (function() {
-  var Control, get_keycode, keycodes, listeners;
-  listeners = [];
-  keycodes = [
-    {
-      key: 'left',
-      code: 37
-    }, {
-      key: 'up',
-      code: 38
-    }, {
-      key: 'right',
-      code: 39
-    }, {
-      key: 'down',
-      code: 40
-    }, {
-      key: 'enter',
-      code: 13
-    }
-  ];
-  get_keycode = function(code) {
-    var keycode, _i, _len;
-    for (_i = 0, _len = keycodes.length; _i < _len; _i++) {
-      keycode = keycodes[_i];
-      if (keycode.code === code) {
-        return keycode;
-      }
-    }
-  };
-  return Control = (function() {
-    function Control() {}
-
-    Control.subscribe = function(listener) {
-      listeners.push(listener);
-      return function() {
-        return unsubscribe(listener);
-      };
-    };
-
-    Control.unsubscribe = function(listener) {
-      return _.remove(listeners, listener);
-    };
-
-    Control.initialize = function() {
-      return document.onkeydown = function(event) {
-        var keycode, listener, _i, _len;
-        if (keycode = get_keycode(event.keyCode)) {
-          for (_i = 0, _len = listeners.length; _i < _len; _i++) {
-            listener = listeners[_i];
-            listener.handle(keycode.key);
-          }
-          event.preventDefault();
-          return false;
-        }
-      };
-    };
-
-    return Control;
-
-  })();
-})(this);
-
-	function iniciarDialog(){
-		$(function(){component.exit.set_active(true);});	
-	}
-
-	function reiniciarTablero(){
-		console.log(component);
-		$(function(){component.exit.set_not_active();});
-		$(function(){component.main_left.set_active(true);});
-		console.log(component);
-	}
-	
-	function iniciarTablero() {
-		
-		component.control.initialize();
-		component.control.subscribe(component.root);
-		
-		component.main_left = extensible.create_vertical('main-left');
-		
-		
-		component.exit = extensible.create_horizontal('exit');
-		
-		component.root
-			.add(extensible.create_horizontal('dialog')
-			    .add(component.exit.set_priority(0)
-			      .add(extensible.create_horizontal('end').set_priority(0)
-			        .add(extensible.create_leaf('Reiniciar-dialog').set_priority(0))
-			        .add(extensible.create_leaf('Salir-dialog').set_priority(1))
-			      )
-			    )
-			);
-		
-		component.root
-		  .add(extensible.create_horizontal('main')
-		    .add(component.main_left.set_priority(0)
-		      .add(extensible.create_horizontal('mentira').set_priority(0)
-		        .add(extensible.create_leaf('Envido').set_priority(0))
-		        .add(extensible.create_leaf('RealEnvido').set_priority(1))
-		        .add(extensible.create_leaf('FaltaEnvido').set_priority(2))
-		      )
-		      .add(extensible.create_horizontal('rabon').set_priority(1)
-		        .add(extensible.create_leaf('IrAlMazo').set_priority(0))
-		        .add(extensible.create_leaf('Truco').set_priority(1))
-		        .add(extensible.create_leaf('reTruco').set_priority(2))
-		        .add(extensible.create_leaf('vale4').set_priority(3))
-		      )
-		      .add(extensible.create_horizontal('respuesta').set_priority(2)
-		        .add(extensible.create_leaf('Quiero').set_priority(0))
-		        .add(extensible.create_leaf('NoQuiero').set_priority(1))
-		        .add(extensible.create_leaf('Reiniciar-left').set_priority(2))
-		      )
-		      
-		    )
-		    .add(extensible.create_horizontal('main-center').set_priority(1)
-		    	.add(extensible.create_leaf('naipe-0').set_priority(0))
-		        .add(extensible.create_leaf('naipe-1').set_priority(1))
-		        .add(extensible.create_leaf('naipe-2').set_priority(2))
-		    )
-		    .add(extensible.create_vertical('main-right').set_priority(2)
-		    	.add(extensible.create_leaf('opciones').set_priority(0))
-		    	.add(extensible.create_horizontal('menu').set_priority(1)
-		    		.add(extensible.create_leaf('Reiniciar-right').set_priority(0))
-		    		.add(extensible.create_leaf('Salir').set_priority(1))
-		    	)
-		    )
-		);
-	}
-
-	iniciarTablero();
-	
-	function sacarCarta(indice){
-		var componentes = component.root.components[0].components[1];
-		console.log(componentes);
-		componentes.remove_leaf("naipe-1");
-		console.log(componentes);
-	}	
 	
 	
 	//******************************************************************
